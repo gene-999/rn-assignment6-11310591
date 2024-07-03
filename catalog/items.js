8ae2ea67c86e9cd218e4c390dae04592332f84f6
gene-999 <eugenebaidoo5@gmail.com>
2024-07-03 00:16:26 +0000

    commit of json containing entire catalog

---
+++ catalog/items.js
@@ -0,0 +1,66 @@
+export const items = [
+    {
+        id: 1,
+        name: "Office Wear",
+        image: require("./assets/dress1.png"),
+        price: "$120",
+        description: "reversible angora cardigan",
+        additional_info: "Office wear for you office"
+    },
+    {
+        id: 2,
+        name: "Black",
+        image: require(`./assets/dress2.png`),
+        price: "$120",
+        description: "reversible angora cardigan",
+        additional_info: "Office wear for you office" 
+    },
+    {
+        id: 3,
+        name: "Church Wear",
+        image: require("./assets/dress3.png"),
+        price: "$120",
+        description: "reversible angora cardigan",
+        additional_info: "Recycle Boucle Knit Cardigan Pink"
+    },
+    {
+        id: 4,
+        name: "Lamerei",
+        image: require(`./assets/dress4.png`),
+        price: "$120",
+        description: "reversible angora cardigan"   ,
+        additional_info: "Recycle Boucle Knit Cardigan Pink"
+    },
+    {
+        id: 5,
+        name: "21WN",
+        image: require(`./assets/dress5.png`),
+        price: "$120",
+        description: "reversible angora cardigan" ,
+        additional_info: "Recycle Boucle Knit Cardigan Pink"  
+    },
+    {
+        id: 6,
+        name: "Lopo",
+        image: require(`./assets/dress6.png`),
+        price: "$120",
+        description: "reversible angora cardigan" ,
+        additional_info: "Recycle Boucle Knit Cardigan Pink"  
+    },
+    {
+        id: 7,
+        name: "21WN",
+        image: require(`./assets/dress7.png`),
+        price: "$120",
+        description: "reversible angora cardigan",
+        additional_info: "Recycle Boucle Knit Cardigan Pink"   
+    },
+    {
+        id: 8,
+        name: "lame",
+        image: require("./assets/dress3.png"),
+        price: "$120",
+        description: "reversible angora cardigan",
+        additional_info: "Recycle Boucle Knit Cardigan Pink"
+    }
+]
